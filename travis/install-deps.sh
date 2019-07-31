c67bc9b94a6c8683dbae7e92c609e707324e1a96
Nicholas Musolino <n.musolino@gmail.com>
2019-07-31 18:00:27 -0400

    Symlink gcc-7 as gcc/g++

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -12,12 +12,18 @@ case "$TRAVIS_OS_NAME" in
 	brew install boost || brew upgrade boost
 	;;
     linux)
+	# Remove/install cmake.  Pip-installed version will be accessible
+	# as /usr/local/bin/cmake
 	sudo apt-get purge cmake
 	sudo apt-get remove -y cmake
 	sudo pip install --upgrade cmake
-	pip show cmake
 	PATH=/usr/local/bin:$PATH which cmake
 	PATH=/usr/local/bin:$PATH cmake --version
+
+	# Create symlinks so that new versions of gcc and g++ will take
+	# priority in PATH.
+	sudo ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
+	sudo ln -s /usr/bin/g++-7 /usr/local/bin/g++
 	;;
     *)
 	echo "install-deps.sh: Unknown TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
