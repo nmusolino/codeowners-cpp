6c249973d60e8ec6923139b2fda34811c8cc8950
Nicholas Musolino <n.musolino@gmail.com>
2020-02-15 16:06:24 -0500

    Install gcc-7 on mac OS X in CI

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-env | grep --extended 'TRAVIS_(OS_NAME|COMPILER)'
-
 case "$TRAVIS_OS_NAME" in
     osx)
-	export HOMEBREW_NO_INSTALL_CLEANUP=1
-	brew install cmake || brew upgrade cmake
-	# Pin the dependents of boost, to avoid a cascade of upgrades
-	# when we upgrade boost.
-	brew pin cgal sfcgal postgis
-	brew install boost || brew upgrade boost
-	;;
+        export HOMEBREW_NO_INSTALL_CLEANUP=1
+        brew install cmake || brew upgrade cmake
+        # Pin the dependents of boost, to avoid a cascade of upgrades
+        # when we upgrade boost.
+        brew pin cgal sfcgal postgis
+        brew install boost || brew upgrade boost
+        case "$TRAVIS_COMPILER" in
+            gcc)
+                brew install gcc-7
+                export CC=gcc-7 CXX=g++-7
+            ;;
+            *)
+                echo "No compiler installation needed for: $TRAVIS_COMPILER"
+        esac
+        ;;
     linux)
-	# Remove/install cmake.  Pip-installed version will be accessible
-	# as /usr/local/bin/cmake
-	sudo apt-get purge cmake
-	sudo apt-get remove -y cmake
-	sudo pip install --upgrade cmake
-	PATH=/usr/local/bin:$PATH which cmake
-	PATH=/usr/local/bin:$PATH cmake --version
-
-	# Create symlinks so that new versions of gcc and g++ will take
-	# priority in PATH.
-	sudo ln -s /usr/bin/gcc-8 /usr/local/bin/gcc
-	sudo ln -s /usr/bin/g++-8 /usr/local/bin/g++
-	;;
+        # Remove/install cmake.  Pip-installed version will be accessible
+        # as /usr/local/bin/cmake
+        sudo apt-get purge cmake
+        sudo apt-get remove -y cmake
+        sudo pip install --upgrade cmake
+        PATH=/usr/local/bin:$PATH which cmake
+        PATH=/usr/local/bin:$PATH cmake --version
+        ;;
     *)
-	echo "install-deps.sh: Unknown TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
-	exit 1
+        echo "install-deps.sh: Unknown TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
+        exit 1
 esac
 
 for program in make cmake $CC $CXX; do
